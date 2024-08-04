6d9dd57ba870fec28981d62905b3de397573a794
jsek <im68889094144@outlook.com>
2024-08-04 12:24:53 +0200

    My backup

---
+++ extensions.sh
@@ -0,0 +1,93 @@
+code --install-extension aaron-bond.better-comments
+code --install-extension adrianwilczynski.namespace
+code --install-extension antfu.iconify
+code --install-extension antfu.unocss
+code --install-extension archsense.architecture-view-nestjs
+code --install-extension be5invis.vscode-custom-css
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension chakrounanas.turbo-console-log
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension cpreston321.nuxt-vscode
+code --install-extension csstools.postcss
+code --install-extension darkriszty.markdown-table-prettify
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dbalas.vscode-html2pug
+code --install-extension denoland.vscode-deno
+code --install-extension eamodio.gitlens
+code --install-extension eliverlara.sweet-vscode-icons
+code --install-extension emmanuelbeziat.vscode-great-icons
+code --install-extension esbenp.prettier-vscode
+code --install-extension fabiospampinato.vscode-projects-plus
+code --install-extension humao.rest-client
+code --install-extension idered.npm
+code --install-extension jeff-hykin.simple-csv-syntax
+code --install-extension lafe.contextualduplicate
+code --install-extension medo64.render-crlf
+code --install-extension mhutchie.git-graph
+code --install-extension mikestead.dotenv
+code --install-extension mohammadbaqer.better-folding
+code --install-extension oven.bun-vscode
+code --install-extension phplasma.csv-to-table
+code --install-extension raillyhugo.one-hunter
+code --install-extension rangav.vscode-thunder-client
+code --install-extension redhat.vscode-xml
+code --install-extension redhat.vscode-yaml
+code --install-extension simonsiefke.svg-preview
+code --install-extension syler.sass-indented
+code --install-extension takumii.markdowntable
+code --install-extension xoofx.scriban
+code --install-extension ybaumes.highlight-trailing-white-spaces
+code --install-extension yoavbls.pretty-ts-errors
+code --install-extension zignd.html-css-class-completion
+
+code --install-extension vue.volar
+code --install-extension maantje.vue-discovery
+code --install-extension dariofuzinato.vue-peek
+code --install-extension sdras.vue-vscode-snippets
+
+code --install-extension mtxr.sqltools
+code --install-extension mtxr.sqltools-driver-mssql
+
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-azure-devops.azure-pipelines
+code --install-extension ms-azuretools.vscode-azurefunctions
+code --install-extension ms-azuretools.vscode-azureresourcegroups
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+
+# code --install-extension wallabyjs.console-ninja
+# code --install-extension mongodb.mongodb-vscode
+# code --install-extension octref.vetur
+
+# Themes
+code --install-extension teabyii.ayu
+code --install-extension geisslerh.ayu-mirage-bold-italic
+code --install-extension samrapdev.outrun
+code --install-extension liviuschera.noctis
+code --install-extension benvalent.bentheme
+code --install-extension hamza-aziane.obsidian-dark
+code --install-extension ceciljacob.code-plus-theme
+code --install-extension eliostruyf.vscode-unnamed-theme
+code --install-extension eugeneego.ee-dark-theme
+code --install-extension helgardrichard.helium-icon-theme
+code --install-extension inci-august.august-themes
+code --install-extension mikaelkristiansson87.react-theme-vscode
+code --install-extension nikitarevenco.dark-cosmos-theme
+code --install-extension origamid.origamid-theme
+code --install-extension unthrottled.doki-theme
+code --install-extension damunga.talos-dark
+code --install-extension pshershov.blueberry-banana
+code --install-extension mvllow.rose-pine
+code --install-extension wicked-labs.wvsc-serendipity
+code --install-extension webrender.synthwave-x-fluoromachine
+code --install-extension kinjalraykarmakar.colors-contrasted
+code --install-extension auracle.coffee-at-2-am
+code --install-extension ajshortt.tokyo-hack
+code --install-extension arthurwhite.white
+code --install-extension atomiks.moonlight
+code --install-extension catppuccin.catppuccin-vsc
+code --install-extension catppuccin.catppuccin-vsc-icons
+code --install-extension darpots.horizons-edge
